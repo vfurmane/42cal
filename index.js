917a0524ecba53c3ef4f33bfae9701c70ff30dc5
valfur03 <valentin.furmanek@gmail.com>
2022-01-14 14:35:33 +0100

    feat: add calendar package

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const ical = require('ical-generator');
 const app = express();
+
+const calendar = ical({name: '42 Events'});
+// calendar.createEvent({
+//     start: '2021-01-01',
+//     summary: 'New Year',
+//     description: 'First day of 2022',
+// });
+
+app.get('/', (req, res) => calendar.serve(res));
 
 app.listen(3000, () => {
 	console.log('Running on http://localhost:3000');
