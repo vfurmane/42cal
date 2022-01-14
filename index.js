2bf862cb9cf5e9f370d5a0492ccc4b3d452f9041
valfur03 <valentin.furmanek@gmail.com>
2022-01-14 18:21:50 +0100

    feat: add authentication

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const ical = require('ical-generator');
+const { ClientCredentials } = require('simple-oauth2');
 const app = express();
+
+app.use(express.json());
+let access_token;
+const callback_url = 'http://localhost:3000/callback';
+
+const config = {
+	client: {
+		id: process.env.CLIENT_ID,
+		secret: process.env.CLIENT_SECRET 
+	},
+	auth: {
+		tokenHost: 'https://api.intra.42.fr',
+		tokenPath: '/oauth/token',
+		authorizePath: '/oauth/authorize'
+	}
+};
+
+const client = new ClientCredentials(config);
+
+const tokenParams = {
+  scope: 'public'
+};
+
+async function checkToken(req, res, next) {
+	if (!access_token) {
+		res.redirect('/auth');
+	}
+	else if (access_token.expired()) {
+		try {
+			const { token } = await client.getToken(tokenParams);
+			access_token = client.createToken(token);
+		} catch (error) {
+			return res.send('error');
+		}
+	}
+	next();
+}
 
 const calendar = ical({name: '42 Events'});
 // calendar.createEvent({
@@ -9,7 +48,29 @@ const calendar = ical({name: '42 Events'});
 //     description: 'First day of 2022',
 // });
 
-app.get('/', (req, res) => calendar.serve(res));
+// app.get('/', (req, res) => calendar.serve(res));
+app.get('/', (req, res) => {
+  res.send('Hello<br><a href="/auth">Log in</a>');
+});
+
+
+app.get('/auth', (req, res) => {
+	res.redirect(`https://api.intra.42.fr/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${callback_url}&response_type=code`);
+});
+
+app.get('/callback', async (req, res) => {
+	try {
+		const { token } = await client.getToken(tokenParams);
+		access_token = client.createToken(token);
+		res.redirect('/api');
+	} catch (error) {
+		return res.send('error');
+	}
+});
+
+app.get('/api', checkToken, (req, res) => {
+	res.send('api here');
+});
 
 app.listen(3000, () => {
 	console.log('Running on http://localhost:3000');
