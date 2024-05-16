494a668e1bdc439c635d9ee169f92a325e1f55fd
vfurmane <vfurmane@student.42.fr>
2024-05-16 18:53:32 +0200

    fix: disable events cache TTL

--- src/common/constants/ft-api-cache.ts
+++ src/common/constants/ft-api-cache.ts
@@ -1,3 +1,4 @@
 export const FT_DEFAULT_EVENTS_LIST = [];
 export const FT_CACHED_EVENTS_CACHE_KEY = 'ft:events:cache';
+export const FT_CACHED_EVENTS_TTL = 0;
 export const FT_EVENTS_CACHE_REVALIDATION_INTERVAL = 50 * 1000;
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -2,7 +2,11 @@ import { Inject, Injectable } from '@nestjs/common';
 import { FtApiService } from '../ft-api/ft-api.service';
 import { ConfigService } from '@nestjs/config';
 import { FT_DEFAULT_CAMPUS_ID } from '../common/constants/ft-api-config';
-import { FT_CACHED_EVENTS_CACHE_KEY, FT_DEFAULT_EVENTS_LIST } from '../common/constants/ft-api-cache';
+import {
+  FT_CACHED_EVENTS_CACHE_KEY,
+  FT_CACHED_EVENTS_TTL,
+  FT_DEFAULT_EVENTS_LIST,
+} from '../common/constants/ft-api-cache';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 
@@ -20,7 +24,7 @@ export class EventsService {
 
   async prefetchAllEvents() {
     const events = await this.ftApiService.findAllEvents();
-    await this.cacheManager.set(FT_CACHED_EVENTS_CACHE_KEY, events);
+    await this.cacheManager.set(FT_CACHED_EVENTS_CACHE_KEY, events, FT_CACHED_EVENTS_TTL);
   }
 
   async findAll() {
