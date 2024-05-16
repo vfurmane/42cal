e723a4d7e78699a1238fa1f446f2111bfa788eee
vfurmane <vfurmane@student.42.fr>
2024-05-16 21:07:29 +0200

    feat: set the PRODID ICS value

--- src/common/constants/calendars-name.ts
+++ src/common/constants/calendars-name.ts
@@ -1,1 +1,3 @@
 export const CALENDAR_NAME_ALL_EVENTS = 'All events at 42';
+
+export const CALENDARS_PRODID = '-//VALFUR//42CAL 42SCHOOL EVENTS';
--- src/common/utils/create-ical-from-events.ts
+++ src/common/utils/create-ical-from-events.ts
@@ -1,9 +1,10 @@
 import ical, { ICalCalendarData } from 'ical-generator';
 import { FindEventsResponseDto } from '../../ft-api/dto/find-events-response.dto';
+import { CALENDARS_PRODID } from '../constants/calendars-name';
 
 export function createIcalFromEvents(config: ICalCalendarData) {
   return (events: FindEventsResponseDto) => {
-    const calendar = ical(config);
+    const calendar = ical({ prodId: CALENDARS_PRODID, ...config });
     events.forEach(({ begin_at, end_at, name, description, location }) => {
       calendar.createEvent({
         start: begin_at,
