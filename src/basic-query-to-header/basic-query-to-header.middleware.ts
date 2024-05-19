474d15f5a495cfc79a7a41761c647bd679841866
vfurmane <vfurmane@student.42.fr>
2024-05-19 12:35:06 +0200

    feat: preserve Authorization header if query param does not exist

--- src/basic-query-to-header/basic-query-to-header.middleware.ts
+++ src/basic-query-to-header/basic-query-to-header.middleware.ts
@@ -5,7 +5,10 @@ import { Request, Response } from 'express';
 @Injectable()
 export class BasicQueryToHeaderMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    req.headers[AUTH_BASIC_HEADER_KEY] = `Basic ${req.query[AUTH_BASIC_QUERY_PARAM_KEY]}`;
+    const queryParam = req.query[AUTH_BASIC_QUERY_PARAM_KEY];
+    if (queryParam !== undefined) {
+      req.headers[AUTH_BASIC_HEADER_KEY] = `Basic ${req.query[AUTH_BASIC_QUERY_PARAM_KEY]}`;
+    }
     next();
   }
 }
