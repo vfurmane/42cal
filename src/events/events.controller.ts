8ccdcdc382da361cd602d47f3e1f075127913ad8
vfurmane <vfurmane@student.42.fr>
2024-05-19 12:25:36 +0200

    feat: add Basic authenticationin front of GET /events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { EventsService } from './events.service.js';
 import { EventsToIcsInterceptor } from '../events-to-ics/events-to-ics.interceptor.js';
 import { CALENDAR_NAME_ALL_EVENTS } from '../common/constants/calendars-name.js';
 import { FindAllEventsDto } from './dto/find-all-events.dto.js';
+import { BasicAuthGuard } from '../auth/basic-auth.guard.js';
 
 @Controller('events')
+@UseGuards(BasicAuthGuard)
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { EventsService } from './events.service.js';
 import { EventsController } from './events.controller.js';
 import { FtModule } from '../ft/ft.module.js';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
+import { BasicQueryToHeaderMiddleware } from '../basic-query-to-header/basic-query-to-header.middleware.js';
 
 @Module({
   imports: [CacheModule.register(), ConfigModule, FtModule],
@@ -11,4 +12,8 @@ import { CacheModule } from '@nestjs/cache-manager';
   providers: [EventsService],
   exports: [EventsService],
 })
-export class EventsModule {}
+export class EventsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(BasicQueryToHeaderMiddleware).forRoutes(EventsController);
+  }
+}
