fb12a45ac5f07ce6db893aa823d2a9dcd6ec039e
vfurmane <vfurmane@student.42.fr>
2024-05-19 12:27:22 +0200

    feat: add BasicQueryToHeaderMiddleware to transform query param to Authorization header

---
+++ src/basic-query-to-header/basic-query-to-header.middleware.ts
@@ -0,0 +1,11 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { AUTH_BASIC_HEADER_KEY, AUTH_BASIC_QUERY_PARAM_KEY } from '../common/constants/auth.js';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class BasicQueryToHeaderMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    req.headers[AUTH_BASIC_HEADER_KEY] = `Basic ${req.query[AUTH_BASIC_QUERY_PARAM_KEY]}`;
+    next();
+  }
+}
---
+++ src/common/constants/auth.ts
@@ -0,0 +1,2 @@
+export const AUTH_BASIC_HEADER_KEY = 'authorization';
+export const AUTH_BASIC_QUERY_PARAM_KEY = 'basic';
