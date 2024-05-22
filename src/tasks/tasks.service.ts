1a6c5b3c8693074df8cd76ed3cefae3f51c4216b
vfurmane <vfurmane@student.42.fr>
2024-05-22 17:58:20 +0200

    refactor: prefetch all events from onModuleInit method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { EventsService } from '../events/events.service.js';
 import { FT_EVENTS_CACHE_REVALIDATION_INTERVAL } from '../common/constants/ft-api-cache.js';
 
 @Injectable()
-export class TasksService {
-  constructor(private readonly eventsService: EventsService) {
-    this.fetchAllFuture42Events();
+export class TasksService implements OnModuleInit {
+  constructor(private readonly eventsService: EventsService) {}
+
+  async onModuleInit() {
+    return this.fetchAllFuture42Events();
   }
 
   @Interval(FT_EVENTS_CACHE_REVALIDATION_INTERVAL)
