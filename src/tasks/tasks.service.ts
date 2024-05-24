ebfbc6f281be5a6d30b1fed7b1c8ea936064d79c
vfurmane <vfurmane@student.42.fr>
2024-05-24 21:16:58 +0200

    feat: prefetch only future events instead of all

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,21 +2,17 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { EventsService } from '../events/events.service.js';
 import { FT_EVENTS_CACHE_REVALIDATION_INTERVAL } from '../common/constants/ft-api-cache.js';
-import { prefetchAll42EventsUntil } from '../common/utils/prefetch-all-42-events-until/prefetch-all-42-events-until.js';
 
 @Injectable()
 export class TasksService implements OnModuleInit {
   constructor(private readonly eventsService: EventsService) {}
 
   async onModuleInit() {
-    return this.eventsService.prefetchAllEvents();
+    return this.eventsService.prefetchAllFutureEvents();
   }
 
   @Interval(FT_EVENTS_CACHE_REVALIDATION_INTERVAL)
-  async fetchAll42Events() {
-    const latestEvent = await this.eventsService.findLatestEvent();
-    await this.eventsService.prefetchAllEvents(
-      latestEvent !== null ? prefetchAll42EventsUntil(latestEvent) : undefined,
-    );
+  async prefetchAllFuture42Events() {
+    await this.eventsService.prefetchAllFutureEvents();
   }
 }
