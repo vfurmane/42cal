9a6b08070d419aac9299ac184527bd653a64bf35
vfurmane <vfurmane@student.42.fr>
2024-05-24 19:44:17 +0200

    feat: add `id` property to 42 events

--- src/ft-api/dto/find-events-response.dto.ts
+++ src/ft-api/dto/find-events-response.dto.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const findEventsResponseSchema = z.array(
   z.object({
+    id: z.number(),
     name: z.string(),
     campus_ids: z.array(z.number()),
     cursus_ids: z.array(z.number()),
