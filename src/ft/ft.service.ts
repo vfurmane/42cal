4b1f2114f3f3e6919c9199155abf92d83ecb3ff4
vfurmane <vfurmane@student.42.fr>
2024-05-24 19:48:04 +0200

    feat: fetch and cache all events instead of just future ones

--- src/ft/ft.service.ts
+++ src/ft/ft.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { findEventsResponseSchema } from '../ft-api/dto/find-events-response.dto.js';
-import { FtApiService } from '../ft-api/ft-api.service.js';
+import { Injectable, Logger } from '@nestjs/common';
+import { FindEventsResponseDto, findEventsResponseSchema } from '../ft-api/dto/find-events-response.dto.js';
+import { FetchUntilFunction, FtApiService } from '../ft-api/ft-api.service.js';
 
 @Injectable()
 export class FtService {
+  private readonly logger = new Logger(FtService.name);
+
   constructor(private readonly ftApiService: FtApiService) {}
 
   async findAllFutureEvents() {
     return this.ftApiService.fetchApiAllPages('v2/events?filter[future]=true', {
       schema: { response: findEventsResponseSchema },
     });
+  }
+
+  async findAllEvents(untilFn?: FetchUntilFunction<FindEventsResponseDto[number]>) {
+    this.logger.verbose(`Finding all events at 42 (untilFn is ${untilFn ? 'set' : 'unset'})`);
+    return this.ftApiService.fetchApiAllPages(
+      'v2/events',
+      {
+        schema: { response: findEventsResponseSchema },
+      },
+      untilFn,
+    );
   }
 
   async findAllEventsByCampusId(campusId: number) {
