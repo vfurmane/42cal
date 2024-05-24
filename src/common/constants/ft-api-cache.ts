e731e9baa702b6ad7f3b7eebb555af2b00cbe3a0
vfurmane <vfurmane@student.42.fr>
2024-05-24 19:41:14 +0200

    feat: fetch a maximum number of pages in fetchApiAllPages method

--- src/common/constants/ft-api-cache.ts
+++ src/common/constants/ft-api-cache.ts
@@ -1,4 +1,5 @@
 export const FT_DEFAULT_EVENTS_LIST = [];
 export const FT_CACHED_EVENTS_CACHE_KEY = 'ft:events:cache';
+export const FT_CACHED_LATEST_EVENT_CACHE_KEY = 'ft:events:latest';
 export const FT_CACHED_EVENTS_TTL = 0;
 export const FT_EVENTS_CACHE_REVALIDATION_INTERVAL = 50 * 1000;
