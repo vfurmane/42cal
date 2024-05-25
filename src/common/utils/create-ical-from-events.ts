928faede9e25b4fa7264b4c217bfa2ad741df88a
vfurmane <vfurmane@student.42.fr>
2024-05-25 08:54:26 +0200

    fix: set event ID using 42 one to prevent event re-creation between requests

--- src/common/utils/create-ical-from-events.ts
+++ src/common/utils/create-ical-from-events.ts
@@ -5,8 +5,9 @@ import { CALENDARS_PRODID } from '../constants/calendars.js';
 export function createIcalFromEvents(config: ICalCalendarData) {
   return (events: FindEventsResponseDto) => {
     const calendar = ical({ prodId: CALENDARS_PRODID, ...config });
-    events.forEach(({ begin_at, end_at, name, description, location }) => {
+    events.forEach(({ id, begin_at, end_at, name, description, location }) => {
       calendar.createEvent({
+        id,
         start: begin_at,
         end: end_at,
         summary: name,
