08760c3e6471209fe4f2d22cb8898c957c335ad6
vfurmane <vfurmane@student.42.fr>
2024-06-08 14:28:48 +0200

    refactor: require only the `kind` property for byRncp function's event parameter

--- src/common/utils/filter-events/by-rncp.ts
+++ src/common/utils/filter-events/by-rncp.ts
@@ -3,7 +3,7 @@ import { FindAllEventsDto } from '../../../events/dto/find-all-events.dto.js';
 import { RNCP_EVENT_KINDS } from '../../constants/events.js';
 
 export function byRncp(rncp: FindAllEventsDto['rncp']) {
-  return (event: FindEventsResponseDto[number]) => {
+  return (event: Pick<FindEventsResponseDto[number], 'kind'>) => {
     if (rncp === undefined) {
       return true;
     }
